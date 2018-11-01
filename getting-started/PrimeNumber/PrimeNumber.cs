9952e133f0b126437f3b4f0046f2c16197aac6f0
mimski <44443424+mimski@users.noreply.github.com>
2018-11-01 23:39:45 +0200

    Add files via upload

---
+++ getting-started/PrimeNumber/PrimeNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class PrimeNumber
+{
+    static void Main()
+    {
+        Console.Write("Enter a positive number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int divider = 2;
+
+        int maxDivider = (int)Math.Sqrt(num);
+
+        bool prime = true;
+
+        while (prime && (divider <= maxDivider))
+        {
+            if (num % divider == 0)
+            {
+                prime = false;
+            }
+            divider++;
+        }
+        Console.WriteLine("Prime? " + prime);
+    }
+}
