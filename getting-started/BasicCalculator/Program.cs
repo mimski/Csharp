27ab7860f87a4121c1d44936de30927528b02c7e
mimski <44443424+mimski@users.noreply.github.com>
2018-11-14 18:57:39 +0200

    Add files via upload

---
+++ getting-started/BasicCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*Write a program to print the result of adding, subtracting,
+multiplying and dividing two numbers typed by the user*/
+
+class BasicCalculator
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+        Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+        Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+        Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+        Console.WriteLine("{0} mod {1} = {2}", num1, num2, num1 % num2);
+    }
+}
