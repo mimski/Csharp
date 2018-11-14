ab22798caffa5c63682038880435c4c07bdf359d
mimski <44443424+mimski@users.noreply.github.com>
2018-11-14 19:20:48 +0200

    Add files via upload

---
+++ getting-started/AverageOfFourNumbers/AverageOfFourNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*Write program to calculate and display 
+the average of four integer numbers entered by the user.*/
+
+class AverageOfFourNumbers
+{
+    static void Main()
+    {
+        int num1, num2, num3, num4;
+
+        Console.Write("Enter number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter number: ");
+        num3 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter number: ");
+        num4 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Average = {0}", (num1 + num2 + num3 + num4) / 4);
+    }
+}
