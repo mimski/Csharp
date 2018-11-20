f41647c1c6f345ea7e4442900a50e990b5d3350b
mimski <44443424+mimski@users.noreply.github.com>
2018-11-20 15:17:51 +0200

    Update AverageOfFourNumbers.cs

--- getting-started/AverageOfFourNumbers.cs
+++ getting-started/AverageOfFourNumbers.cs
@@ -9,13 +9,13 @@ class AverageOfFourNumbers
     {
         int num1, num2, num3, num4;
 
-        Console.Write("Enter number: ");
+        Console.Write("Enter a number: ");
         num1 = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter number: ");
+        Console.Write("Enter a number: ");
         num2 = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter number: ");
+        Console.Write("Enter a number: ");
         num3 = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter number: ");
+        Console.Write("Enter a number: ");
         num4 = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine("Average = {0}", (num1 + num2 + num3 + num4) / 4);
