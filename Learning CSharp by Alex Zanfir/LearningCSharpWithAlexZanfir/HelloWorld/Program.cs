65fc1222fceb5789563708f2b162c93465eaaab9
mimski <maria.bg.1989@gmail.com>
2020-05-23 19:14:09 +0300

    Configure input

--- Learning CSharp by Alex Zanfir/LearningCSharpWithAlexZanfir/HelloWorld/Program.cs
+++ Learning CSharp by Alex Zanfir/LearningCSharpWithAlexZanfir/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var anything = "My String";
+            var anything = Console.ReadLine();
             Console.WriteLine(anything);
         }
     }
