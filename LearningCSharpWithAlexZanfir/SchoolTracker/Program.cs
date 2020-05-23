c6b2a2648a1259f048501d5d9978389be413d416
mimski <maria.bg.1989@gmail.com>
2020-05-23 20:22:08 +0300

    Foreach loop

--- LearningCSharpWithAlexZanfir/SchoolTracker/Program.cs
+++ LearningCSharpWithAlexZanfir/SchoolTracker/Program.cs
@@ -9,10 +9,13 @@ namespace SchoolTracker
             var studentGrades = new int[] { 70, 77, 45, 88, 53 };
 
             studentGrades[0] = 80;
-            studentGrades[1] = 77;
+            studentGrades[1] = 90;
             studentGrades[4] = 77;
 
-            Console.WriteLine(studentGrades[4]);
+            foreach (var studentGrade in studentGrades)
+            {
+                Console.WriteLine(studentGrade);
+            }
         }
     }
 }
