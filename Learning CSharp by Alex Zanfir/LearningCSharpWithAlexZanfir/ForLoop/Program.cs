87f0b99506296d05cd5de616c8efd13793e50eee
mimski <maria.bg.1989@gmail.com>
2020-05-23 19:51:08 +0300

    For loop

---
+++ Learning CSharp by Alex Zanfir/LearningCSharpWithAlexZanfir/ForLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 1;
+
+            while (count <= 10)
+            {
+                Console.WriteLine(count);
+                count++;
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
