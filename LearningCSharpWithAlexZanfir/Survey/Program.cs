fca48ce6de009f01bb419078a5fc841192c8e1c0
mimski <maria.bg.1989@gmail.com>
2020-05-24 01:05:42 +0300

    Refactor Survey

--- LearningCSharpWithAlexZanfir/Survey/Program.cs
+++ LearningCSharpWithAlexZanfir/Survey/Program.cs
@@ -4,34 +4,48 @@ namespace Survey
 {
     class Program
     {
+        class Data
+        {
+            public string Name;
+            public int Age;
+            public string Month;
+
+            public void Display()
+            {
+                Console.WriteLine("Your name is: {0}", Name);
+                Console.WriteLine("Your age is: {0}", Age);
+                Console.WriteLine("Your birth month is: {0}", Month);
+
+                // Zodiac accuracy is not the biggest concern for this exercise
+                if (Month == "march")
+                {
+                    Console.WriteLine("you are an Aries");
+                }
+                else if (Month == "april")
+                {
+                    Console.WriteLine("you are a Taurus");
+                }
+                else if (Month == "may")
+                {
+                    Console.WriteLine("you are a Gemini");
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            var data = new Data();
+
             Console.WriteLine("What is your name?");
-            var name = TryAnswer();
+            data.Name = TryAnswer();
 
             Console.WriteLine("What is your age?");
-            var age = TryAnswer();
+            data.Age = int.Parse(TryAnswer());
 
             Console.WriteLine("What month were you born in?");
-            var month = TryAnswer();
+            data.Month = TryAnswer();
 
-            Console.WriteLine("Your name is: {0}", name);
-            Console.WriteLine("Your age is: {0}", age);
-            Console.WriteLine("Your birth month is: {0}", month);
-
-            // Zodiac accuracy is not the biggest concern for this exercise
-            if (month == "march")
-            {
-                Console.WriteLine("you are an Aries");
-            }
-            else if (month == "april")
-            {
-                Console.WriteLine("you are a Taurus");
-            }
-            else if (month == "may")
-            {
-                Console.WriteLine("you are a Gemini");
-            }
+            data.Display();
         }
 
         static string TryAnswer()
