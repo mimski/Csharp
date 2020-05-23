2918e41019f301e72ea0c3c84dea80f69d4557ae
mimski <maria.bg.1989@gmail.com>
2020-05-23 20:03:18 +0300

    Loop Counting

--- Learning CSharp by Alex Zanfir/LearningCSharpWithAlexZanfir/ForLoop/Program.cs
+++ Learning CSharp by Alex Zanfir/LearningCSharpWithAlexZanfir/ForLoop/Program.cs
@@ -18,6 +18,26 @@ namespace ForLoop
             {
                 Console.WriteLine(i);
             }
+
+            #region Challenge: Loop Counting
+            /* Count from 1 to 10 
+             * and then back to 1, 
+             * and repeat five times
+             * Do not use if statements
+             */
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int x = 0; x <= 10; x++)
+                {
+                    Console.WriteLine(x);
+                }
+                for (int y = 10; y >= 1; y--)
+                {
+                    Console.WriteLine(y);
+                }
+            }
+            #endregion
         }
     }
 }
