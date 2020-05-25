d596daa75741491d10010cb7b567e7f2dcbaea66
mimski <maria.bg.1989@gmail.com>
2020-05-25 17:32:25 +0300

    Working with the out parameter

---
+++ LearningCSharpWithAlexZanfir/OutReference/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OutReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 2;
+
+            Console.WriteLine(x);
+
+            Five(x);
+
+            Console.WriteLine(x);
+
+            Six(out x);
+
+            Console.WriteLine(x);
+        }
+
+        static void Five(int a)
+        {
+            a = 5;
+
+            Console.WriteLine(a);
+        }
+
+        static void Six(out int a)
+        {
+            a = 6;
+
+            Console.WriteLine(a);
+        }
+    }
+}
