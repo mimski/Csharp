29644e4ef1d26b0f7d23da0a84d9456ab6d58d52
mimski <maria.bg.1989@gmail.com>
2020-05-25 17:36:50 +0300

    Working with ref parameter

--- LearningCSharpWithAlexZanfir/OutReference/Program.cs
+++ LearningCSharpWithAlexZanfir/OutReference/Program.cs
@@ -17,6 +17,10 @@ namespace OutReference
             Six(out x);
 
             Console.WriteLine(x);
+
+            Double(ref x);
+
+            Console.WriteLine(x);
         }
 
         static void Five(int a)
@@ -29,6 +33,13 @@ namespace OutReference
         static void Six(out int a)
         {
             a = 6;
+
+            Console.WriteLine(a);
+        }
+
+        static void Double(ref int a)
+        {
+            a *= 2;
 
             Console.WriteLine(a);
         }
