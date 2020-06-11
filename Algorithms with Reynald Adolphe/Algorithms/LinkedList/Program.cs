89973f81ed96db8d9e40caa10e373f63d30b9e25
mimski <maria.bg.1989@gmail.com>
2020-06-11 22:05:25 +0300

    Implementation of Singly Linked List

---
+++ Algorithms with Reynald Adolphe/Algorithms/LinkedList/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+            singlyLinkedList.InsertFirst(100);
+            singlyLinkedList.InsertFirst(50);
+            singlyLinkedList.InsertFirst(19);
+            singlyLinkedList.InsertFirst(89);
+
+            singlyLinkedList.InsertLast(100000);
+            singlyLinkedList.InsertLast(77);
+
+            singlyLinkedList.DisplayList();
+
+            singlyLinkedList.DeleteFirst();
+            singlyLinkedList.DisplayList();
+        }
+    }
+
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public void DisplayNode()
+        {
+            Console.WriteLine("<" + data + ">");
+        }
+    }
+
+    public class SinglyLinkedList 
+    {
+        private Node first;
+
+        public bool IsEmpty()
+        {
+            return (first == null);
+        }
+
+        public void InsertFirst(int data)
+        {
+            Node newNode = new Node();
+            newNode.data = data;
+            newNode.next = first;
+            first = newNode;
+        }
+
+        public Node DeleteFirst()
+        {
+            Node temp = first;
+            first = first.next;
+            return temp;
+        }
+
+        public void DisplayList()
+        {
+            Console.WriteLine("List (first -- last) ");
+            
+            Node current = first;
+
+            while (current != null)
+            {
+                current.DisplayNode();
+                current = current.next;
+            }
+
+            Console.WriteLine();
+        }
+
+        public void InsertLast(int data)
+        {
+            Node current = first;
+
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            Node newNode = new Node();
+            newNode.data = data;
+            current.next = newNode;
+        }
+    }
+}
